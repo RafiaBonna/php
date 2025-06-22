457ce419e4056a6ac9d7fdd7acdecc553f452bec
RafiaBonna <rafiaakterbonna@gmail.com>
2025-06-22 13:27:31 +0600

    my

---
+++ php-basic/constant.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Constant</h1>
+    <!-- define(name,value); -->
+     <?php
+     define('ANIMALS',"This is const");
+     echo ANIMALS;
+    //  outputs "cat
+    //  echo ANIMALS[2]; outputs"cat"
+    echo "<br>";
+    const TANT ='Hello World';
+    // const CONSTANT ='Hello';already define
+    echo TANT;
+    const ANOTHER_CONST ='CONSTANT'
+    ?>
+</body>
+</html>
