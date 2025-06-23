34ca09dd52d8445380139979ba9e32e216a1400e
RafiaBonna <rafiaakterbonna@gmail.com>
2025-06-23 10:42:35 +0600

    alam

--- php-basic/function/arrow_function.php
+++ php-basic/function/arrow_function.php
@@ -3,19 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Arrow function</title>
 </head>
 <body>
     <?php
+    // anonymous function
     $add=function($a,$b){
         return $a+$b;
     };
     echo $add(40,80);
     echo "<br>";
+
+
+
     // arrow function
     $addArrow=fn($a,$b)=>$a+$b;
     echo $addArrow(50,40);
         echo "<br>";
+        $calender =fn($m,$r) =>$m*$r;
+        echo $calender(20,5);
     ?> 
 </body>
 </html>
