5e61363ff5daad2594c1dc7f08e85493f110d58d
RafiaBonna <rafiaakterbonna@gmail.com>
2025-07-19 23:08:49 +0600

    alam

---
+++ php-basic/php_home/finalevidence/fileupload.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+// লগইন না করলে login.php তে ফেরত
+if (!isset($_SESSION["sname"])) {
+    header("Location: login.php");
+    exit();
+}
+
+// সেটআপ
+$imgDir = "pic/";
+$allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+$maxSizeKb = 400;
+$uploadedImages = [];
+
+// ফাইল আপলোড চেক
+if (isset($_POST['submit'])) {
+    foreach ($_FILES['file']['name'] as $i => $name) {
+        $tmp = $_FILES['file']['tmp_name'][$i];
+        $type = $_FILES['file']['type'][$i];
+        $sizeKb = $_FILES['file']['size'][$i] / 1024;
+
+        if ($sizeKb > $maxSizeKb) {
+            echo "<p style='color:red;'>$name is too big!</p>";
+            continue;
+        }
+
+        if (!in_array($type, $allowedTypes)) {
+            echo "<p style='color:red;'>$name type not allowed!</p>";
+            continue;
+        }
+
+        if (move_uploaded_file($tmp, $imgDir . $name)) {
+            $uploadedImages[] = $name;
+        }
+    }
+}
+?>
+
+<!-- HTML PART -->
+<!DOCTYPE html>
+<html>
+<head><title>Upload</title></head>
+<body>
+    <h2>Upload Photos</h2>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="file[]" multiple required><br>
+        <input type="submit" name="submit" value="Upload">
+    </form>
+
+    <?php if (!empty($uploadedImages)): ?>
+        <h3>Uploaded Images</h3>
+        <?php foreach ($uploadedImages as $img): ?>
+            <img src="pic/<?php echo $img; ?>" width="150">
+        <?php endforeach; ?>
+    <?php endif; ?>
+
+    <br><br>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
---
+++ php-basic/php_home/finalevidence/login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+// যদি ফর্ম সাবমিট হয়
+if (isset($_POST["Login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // ইউজার ডেটা ফাইল থেকে পড়া
+    $file = file("dataa.txt");
+
+    // লাইন ধরে ধরে মিলিয়ে দেখা
+    foreach ($file as $line) {
+        list($savedUser, $savedPass) = explode(",", trim($line));
+        
+        if ($username === trim($savedUser) && $password === trim($savedPass)) {
+            $_SESSION["sname"] = $username; // লগইন সফল
+            header("Location: fileupload.php");
+            exit();
+        }
+    }
+
+    $msg = "Invalid username or password!";
+}
+?>
+<!-- HTML Form -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <?php if (isset($msg)) echo "<p style='color:red;'>$msg</p>"; ?>
+    <form method="post">
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        <input type="submit" name="Login" value="Log In">
+    </form>
+</body>
+</html>
